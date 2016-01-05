15eb4bc94d81703ed9df756c9bd291ecbebe99f6
Peter Abolins <peterabolins@users.noreply.github.com>
2016-01-05 18:36:05 +0200

    Update 30Days_0.hs

--- 30Days_0.hs
+++ 30Days_0.hs
@@ -1,5 +1,12 @@
 -- Language: Haskell
 --
+-- 30 Days of Code
+-- Day 0: Print Hello World!
+--
+-- For this challenge, all you have to do is print the following two lines:
+--     Hello World. 
+--     Welcome to 30 Days of Code.
+
 main = do
     putStrLn "Hello World."
     putStrLn "Welcome to 30 Days of Code."
