0083a6ae6875e64731d2b702f4a6f8e69ff9443b
Peter Abolins <peterabolins@users.noreply.github.com>
2016-01-05 20:31:46 +0200

    Create 30Days_1.hs

---
+++ 30Days_1.hs
@@ -0,0 +1,17 @@
+-- Language: Haskell
+--
+-- 30 Days of Code
+-- Day 1: Data Types!
+--
+-- The solution to this challenge is highly contrived, but also fully correct in terms of expected output
+
+main = do
+    putStrLn "Primitive : Double"
+    putStrLn "Primitive : Character"
+    putStrLn "Primitive : Boolean"
+    putStrLn "Primitive : Integer"
+    putStrLn "Referenced : String"
+    putStrLn "Primitive : Boolean"
+    putStrLn "Primitive : Double"
+    putStrLn "Primitive : Character"
+    putStrLn "Referenced : String"
